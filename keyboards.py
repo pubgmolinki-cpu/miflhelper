7c8ee940b8550db14a795ce6fa553207aeb6001f
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-11 20:15:18 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,9 +5,6 @@ from aiogram.types import (
     InlineKeyboardButton
 )
 
-# =======================
-# ГЛАВНОЕ МЕНЮ (НИЖНИЕ КНОПКИ)
-# =======================
 
 def main_menu():
 
@@ -25,48 +22,58 @@ def main_menu():
         resize_keyboard=True
     )
 
-# =======================
-# ЛИГИ (INLINE)
-# =======================
 
 def leagues_keyboard():
 
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="MIFL", callback_data="league_MIFL")
+                InlineKeyboardButton(
+                    text="MIFL",
+                    callback_data="league_MIFL"
+                )
             ],
             [
-                InlineKeyboardButton(text="Кубок Меридианы", callback_data="league_MERIDIAN")
+                InlineKeyboardButton(
+                    text="Кубок Меридианы",
+                    callback_data="league_MERIDIAN"
+                )
             ],
             [
-                InlineKeyboardButton(text="Кубок Первенства", callback_data="league_FIRST")
+                InlineKeyboardButton(
+                    text="Кубок Первенства",
+                    callback_data="league_FIRST"
+                )
             ]
         ]
     )
 
-# =======================
-# ТУРЫ (INLINE)
-# =======================
 
 def tours_keyboard(league):
 
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="1 тур", callback_data=f"tour_{league}_1"),
-                InlineKeyboardButton(text="2 тур", callback_data=f"tour_{league}_2")
+                InlineKeyboardButton(
+                    text="1 тур",
+                    callback_data=f"tour_{league}_1"
+                ),
+
+                InlineKeyboardButton(
+                    text="2 тур",
+                    callback_data=f"tour_{league}_2"
+                )
             ],
             [
-                InlineKeyboardButton(text="3 тур", callback_data=f"tour_{league}_3"),
-                InlineKeyboardButton(text="4 тур", callback_data=f"tour_{league}_4")
-            ],
-            [
-                InlineKeyboardButton(text="5 тур", callback_data=f"tour_{league}_5"),
-                InlineKeyboardButton(text="6 тур", callback_data=f"tour_{league}_6")
-            ],
-            [
-                InlineKeyboardButton(text="7 тур", callback_data=f"tour_{league}_7")
+                InlineKeyboardButton(
+                    text="3 тур",
+                    callback_data=f"tour_{league}_3"
+                ),
+
+                InlineKeyboardButton(
+                    text="4 тур",
+                    callback_data=f"tour_{league}_4"
+                )
             ]
         ]
     )
