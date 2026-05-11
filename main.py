7d14d8b79f6c71d5a76ef080685fd7d75e1df87f
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-11 13:43:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,33 @@ from database import (
 
 from webserver import run_web
 
-from handlers.start import router as start_router
-from handlers.matches import router as matches_router
-from handlers.table import router as table_router
-from handlers.about import router as about_router
-from handlers.admin import router as admin_router
+from handlers.start import (
+    router as start_router
+)
+
+from handlers.matches import (
+    router as matches_router
+)
+
+from handlers.table import (
+    router as table_router
+)
+
+from handlers.about import (
+    router as about_router
+)
+
+from handlers.admin import (
+    router as admin_router
+)
+
+from handlers.stats import (
+    router as stats_router
+)
+
+from handlers.back import (
+    router as back_router
+)
 
 bot = Bot(BOT_TOKEN)
 
@@ -28,10 +50,13 @@ dp.include_router(matches_router)
 dp.include_router(table_router)
 dp.include_router(about_router)
 dp.include_router(admin_router)
+dp.include_router(stats_router)
+dp.include_router(back_router)
 
 async def main():
 
     await connect_db()
+
     await create_tables()
 
     print("MIFL HELPER STARTED")
