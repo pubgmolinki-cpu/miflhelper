044cc91f1b5421c675b6cff08a919a53a1e89c95
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-11 12:43:09 +0400

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,28 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from keyboards import main_menu
+
+router = Router()
+
+@router.message(Command("start"))
+async def start(message: Message):
+
+    text = (
+
+        "🔥 <b>Добро пожаловать в MIFL HELPER!</b>\n\n"
+
+        "Здесь ты можешь:\n"
+        "• смотреть матчи\n"
+        "• смотреть таблицу\n"
+        "• смотреть статистику\n\n"
+
+        "Приятного использования 🔥"
+
+    )
+
+    await message.answer(
+        text,
+        reply_markup=main_menu()
+    )
