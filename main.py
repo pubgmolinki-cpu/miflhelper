80c60c25421107226e657a9d2aa5f97d22cc61ce
pubgmolinki-cpu <pubgmolinki@gmail.com>
2026-05-11 20:14:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,28 +9,23 @@ from database import connect_db, create_tables
 from webserver import run_web
 
 from handlers.start import router as start_router
-from handlers.matches import router as matches_router
-from handlers.table import router as table_router
-from handlers.about import router as about_router
-from handlers.admin import router as admin_router
-from handlers.stats import router as stats_router
+from handlers.menu import router as menu_router
+from handlers.upload import router as upload_router
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start_router)
-dp.include_router(matches_router)
-dp.include_router(table_router)
-dp.include_router(about_router)
-dp.include_router(admin_router)
-dp.include_router(stats_router)
+dp.include_router(menu_router)
+dp.include_router(upload_router)
+
 
 async def main():
 
     await connect_db()
     await create_tables()
 
-    print("MIFL BOT STARTED")
+    print("BOT STARTED")
 
     await dp.start_polling(bot)
 
@@ -38,4 +33,5 @@ async def main():
 if __name__ == "__main__":
 
     threading.Thread(target=run_web).start()
+
     asyncio.run(main())
